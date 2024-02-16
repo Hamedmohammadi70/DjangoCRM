899a66f39295060a0ea8f1032cedf1159334b7ed
Hamedmohammadi70 <Hamedmohammadi741@gmail.com>
2024-02-16 12:39:06 +0330

    registration

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
 	#chek to see if logging in
@@ -19,6 +20,7 @@ def home(request):
 	else:
 	     return render(request, 'home.html', {})
 
+
 def logout_user(request):
 
     logout(request)
@@ -26,5 +28,21 @@ def logout_user(request):
     messages.success(request, "You Have Been logged Out...")
     return redirect('home')
 	
-def register_user(request):	
-	return render(request, 'register.html', {})
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+	
